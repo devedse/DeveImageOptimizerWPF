812bd45626ac0dc3230b16e90967e848dab49f73
Devedse <davydavidse@gmail.com>
2017-07-25 18:15:55 +0200

    Should fix versioning

--- Scripts/set-version.ps1
+++ Scripts/set-version.ps1
@@ -1,13 +1,5 @@
 $ReleaseVersionNumber = $env:APPVEYOR_BUILD_VERSION
-$PreReleaseName = ''
-
-If($env:APPVEYOR_PULL_REQUEST_NUMBER -ne $null) {
-  $PreReleaseName = '-PR-' + $env:APPVEYOR_PULL_REQUEST_NUMBER
-} ElseIf($env:APPVEYOR_REPO_BRANCH -ne 'master' -and -not $env:APPVEYOR_REPO_BRANCH.StartsWith('release')) {
-  $PreReleaseName = '-' + $env:APPVEYOR_REPO_BRANCH
-} Else {
-  $PreReleaseName = '-CI'
-}
+$PreReleaseName = '.0'
 
 $PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
 $ScriptDir = Split-Path -Path $PSScriptFilePath -Parent
@@ -18,11 +10,12 @@ $re1 = [regex]"(?<=\[assembly: AssemblyVersion\(`")1.0.0.0(?=`")"
 $re2 = [regex]"(?<=\[assembly: AssemblyFileVersion\(`")1.0.0.0(?=`")"
 #$re = [regex]"(?<=<Version>).*(?=<\/Version>)"
 
-Write-Host "ProjectJson Path: $ProjectJsonPath"
+Write-Host "AssemblyInfo Path: $assemblyInfoPath"
 Write-Host "Writing version: $ReleaseVersionNumber$PreReleaseName"
 #Write-Host "Regex 1: $re1"
 #Write-Host "Regex 2: $re2"
-Write-Host "Regex: $re"
+Write-Host "Regex1: $re1"
+Write-Host "Regex2: $re2"
  
 #$re.Replace([string]::Join("`n", (Get-Content -Path $ProjectJsonPath)), "$ReleaseVersionNumber$PreReleaseName", 1) | Set-Content -Path $ProjectJsonPath -Encoding UTF8
 $re1.Replace([string]::Join("`n", (Get-Content -Path $assemblyInfoPath)), "$ReleaseVersionNumber$PreReleaseName", 1) | Set-Content -Path $assemblyInfoPath -Encoding UTF8
