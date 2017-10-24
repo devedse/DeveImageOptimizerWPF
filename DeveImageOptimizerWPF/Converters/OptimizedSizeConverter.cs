b64a4b72809ed87f6944a854bd439fe33c65255b
Devedse <davydavidse@gmail.com>
2017-10-24 23:15:05 +0200

    Updated nuget packages

--- DeveImageOptimizerWPF/Converters/OptimizedSizeConverter.cs
+++ DeveImageOptimizerWPF/Converters/OptimizedSizeConverter.cs
@@ -11,6 +11,10 @@ namespace DeveImageOptimizerWPF.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (value == null)
+            {
+                return string.Empty;
+            }
             var ofr = (OptimizedFileResult)value;
             return ValuesToStringHelper.BytesToString(ofr.OriginalSize - ofr.OptimizedSize);
         }
