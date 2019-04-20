1cbeb032178bbd94e7fc0b0c663f988168b8a2e9
Devedse <davydavidse@gmail.com>
2019-04-20 17:35:47 +0200

    Added logpath

--- DeveImageOptimizerWPF/State/ProcessingState/FilesProcessingState.cs
+++ DeveImageOptimizerWPF/State/ProcessingState/FilesProcessingState.cs
@@ -1,18 +1,29 @@
-﻿using DeveImageOptimizer.State;
+﻿using DeveImageOptimizer.Helpers;
+using DeveImageOptimizer.State;
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 
 namespace DeveImageOptimizerWPF.State.MainWindowState
 {
     public class FilesProcessingState : INotifyPropertyChanged, IFilesProcessedListener
     {
+        private readonly string _logPath;
+
         public ObservableCollection<OptimizedFileResult> ProcessedFiles { get; set; } = new ObservableCollection<OptimizedFileResult>();
         public OptimizedFileResult SelectedProcessedFile { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public FilesProcessingState()
+        {
+            _logPath = Path.Combine(FolderHelperMethods.EntryAssemblyDirectory.Value, "log.txt");
+        }
+
         public void AddProcessedFile(OptimizedFileResult optimizedFileResult)
         {
+            File.AppendAllText(_logPath, $"{optimizedFileResult.OptimizationResult}|{optimizedFileResult.Path}{Environment.NewLine}");
             ProcessedFiles.Add(optimizedFileResult);
             OnPropertyChanged(nameof(ProcessedFiles));
         }
