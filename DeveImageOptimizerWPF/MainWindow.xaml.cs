a74193144a70bb03c639ced5b48912d9afe8c4b2
Devedse <davydavidse@gmail.com>
2019-11-29 00:14:36 +0100

    Added version to title bar

--- DeveImageOptimizerWPF/MainWindow.xaml.cs
+++ DeveImageOptimizerWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,8 @@ namespace DeveImageOptimizerWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Title = $"DeveImageOptimizer {Assembly.GetEntryAssembly().GetName().Version}";
         }
     }
 }
