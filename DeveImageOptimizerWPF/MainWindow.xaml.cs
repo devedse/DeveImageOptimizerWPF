de8475c293478e1ab937989057ece1b0a49010d4
Devedse <davydavidse@gmail.com>
2019-11-29 00:51:19 +0100

    Removed unused usings

--- DeveImageOptimizerWPF/MainWindow.xaml.cs
+++ DeveImageOptimizerWPF/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DeveImageOptimizerWPF
 {
@@ -25,7 +12,7 @@ namespace DeveImageOptimizerWPF
         {
             InitializeComponent();
 
-            this.Title = $"DeveImageOptimizer {Assembly.GetEntryAssembly().GetName().Version}";
+            Title = $"DeveImageOptimizer {Assembly.GetEntryAssembly().GetName().Version}";
         }
     }
 }
