d5c76121a6918cf1aa513140579850cfc7becce1
Devedse <davydavidse@gmail.com>
2019-11-29 23:01:56 +0100

    Changed max degree of parallelism

--- DeveImageOptimizerWPF/ViewModel/SettingsViewModel.cs
+++ DeveImageOptimizerWPF/ViewModel/SettingsViewModel.cs
@@ -31,7 +31,7 @@ namespace DeveImageOptimizerWPF.ViewModel
             SaveCommand = new RelayCommand(SaveCommandImp, () => true);
             ResetToDefaultsCommand = new RelayCommand(ResetToDefaultsCommandImpl, () => true);
 
-            MaxParallelismChoices = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            MaxParallelismChoices = Enumerable.Range(1, Environment.ProcessorCount).ToList();
             AvailableLogLevels = new List<int>() { 0, 1, 2, 3, 4 };
 
             AvailableStorageModes = Enum.GetValues(typeof(RemembererSettings)).Cast<RemembererSettings>().ToList();
