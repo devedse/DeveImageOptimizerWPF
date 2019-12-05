b4127c440acb01780a292cb8cee29dbacc239aff
Devedse <davydavidse@gmail.com>
2019-12-05 15:56:54 +0100

    Only push from master branch

--- CreateChocoPackage/PushPackage.ps1
+++ CreateChocoPackage/PushPackage.ps1
@@ -1,7 +1,13 @@
 $ErrorActionPreference = "Stop"
 
-$invocation = (Get-Variable MyInvocation).Value
-$directorypath = Split-Path $invocation.MyCommand.Path
+if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
+	Write-Host "Pushing to Chocolatey..."
 
-$nupkgFile = (Get-ChildItem "$($directorypath)\*" -include *.nupkg).FullName
-choco push $nupkgFile -s "https://push.chocolatey.org/" -k $env:CHOCOLATEYKEY
+	$invocation = (Get-Variable MyInvocation).Value
+	$directorypath = Split-Path $invocation.MyCommand.Path
+
+	$nupkgFile = (Get-ChildItem "$($directorypath)\*" -include *.nupkg).FullName
+	choco push $nupkgFile -s "https://push.chocolatey.org/" -k $env:CHOCOLATEYKEY
+} else {
+	Write-Host "Skipping Chocolatey Push because we're not on the master branch"
+}
