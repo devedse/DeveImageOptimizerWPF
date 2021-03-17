9a725bc0c0b1effd0893caa7b13367db376658e2
Devedse <davydavidse@gmail.com>
2021-03-18 00:04:31 +0100

    This should fix preview locking up files

---
+++ DeveImageOptimizerWPF/Converters/StringToImageSourceConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DeveImageOptimizerWPF.Converters
+{
+    [ValueConversion(typeof(string), typeof(ImageSource))]
+    public class StringToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string valueString))
+            {
+                return null;
+            }
+            try
+            {
+                //BitmapImage image = new BitmapImage();
+                //image.BeginInit();
+                //image.CacheOption = BitmapCacheOption.OnLoad;
+                //image.UriSource = new Uri(valueString);
+                //image.EndInit();
+                //return image;
+                //imgThumbnail.Source = image;
+                ImageSource image = BitmapFrame.Create(new Uri(valueString), BitmapCreateOptions.IgnoreImageCache, BitmapCacheOption.OnLoad);
+                return image;
+            }
+            catch { return null; }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
