ac8e3f8f193587bedc41fdc62abfa47db4b4bee4
Devedse <davydavidse@gmail.com>
2021-04-27 22:58:40 +0200

    Check master branch in githubactionsbuild.yml

--- CreateChocoPackage/PushPackage.ps1
+++ CreateChocoPackage/PushPackage.ps1
@@ -5,14 +5,10 @@ Param(
 
 $ErrorActionPreference = "Stop"
 
-if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
-	Write-Host "Pushing to Chocolatey..."
+Write-Host "Pushing to Chocolatey..."
 
-	$invocation = (Get-Variable MyInvocation).Value
-	$directorypath = Split-Path $invocation.MyCommand.Path
+$invocation = (Get-Variable MyInvocation).Value
+$directorypath = Split-Path $invocation.MyCommand.Path
 
-	$nupkgFile = (Get-ChildItem "$($directorypath)\*" -include *.nupkg).FullName
-	choco push $nupkgFile -s "https://push.chocolatey.org/" -k $chocolateykey
-} else {
-	Write-Host "Skipping Chocolatey Push because we're not on the master branch"
-}
+$nupkgFile = (Get-ChildItem "$($directorypath)\*" -include *.nupkg).FullName
+choco push $nupkgFile -s "https://push.chocolatey.org/" -k $chocolateykey
