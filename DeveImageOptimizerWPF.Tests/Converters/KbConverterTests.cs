affd6a50d4c7fde0f9dc3a07438677143d9b5137
Devedse <davydavidse@gmail.com>
2021-04-27 11:42:19 +0200

    Added working unit test + fixed csproj + fixed release

---
+++ DeveImageOptimizerWPF.Tests/Converters/KbConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using DeveImageOptimizerWPF.Converters;
+using System.Globalization;
+using Xunit;
+
+namespace DeveImageOptimizerWPF.Tests.Converters
+{
+    public class KbConverterTests
+    {
+        [Fact]
+        public void KbToString()
+        {
+            //Arrange
+            var kbConverter = new KbConverter();
+
+            //Act
+            var result = kbConverter.Convert(123456789L, null, null, CultureInfo.InvariantCulture);
+
+            //Assert
+            Assert.Equal("117,7MB", result.ToString());
+        }
+    }
+}
